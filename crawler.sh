2ed3e792a7e4c20a2426c4b8f5bfea9a3e36be9b
yann <yann.boisselier@gmail.com>
2017-03-08 19:25:06 +0100

    ajout de la première version du crawler en bash de tous les releves de temps

---
+++ crawler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for CHAINE in 1 2 3 4 5 6 8 14 15 16 19 24 26 27 101 102 103 104 105 106 107 108 109 110
+do
+  echo "chaine $CHAINE"
+  for VARIABLE in 1 2 3 4
+  do
+    FILE="http://www.csa.fr/csaelections/consultereleve/$CHAINE/$VARIABLE/1?csv=1"
+    echo "fil $FILE"
+    DESTINATION="src/releves/$CHAINE/$VARIABLE/releve-$VARIABLE.csv";
+    curl $FILE --create-dirs -o $DESTINATION;
+    sleep .$[ ( $RANDOM % 4 ) + 1 ]s
+  done
+done
+
