d1e51870c4d6457fc778bb649f6a266430813366
yann <yann.boisselier@gmail.com>
2017-03-09 11:17:03 +0100

    remove unused file

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ContentType=`curl -I http://www.csa.fr/csaelections/consultereleve/1/1/1\?csv\=1 -s | grep "Content-Type"`
-if [[ $ContentType =~ "application/force-download" ]]; then
-    echo $ContentType
-fi
-
-
