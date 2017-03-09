4930b0c8aaf4ee1f5e5ce755ddc66dcc8e9d0628
yann <yann.boisselier@gmail.com>
2017-03-09 17:42:05 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-python3 builders/python_builder/Main.py
+python3 processors/python_builder/Main.py
 node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t src/rapports/2017-02-01--2017-02-26/originaux/Chaines\ Gen\ -\ Période\ du\ 1er\ au\ 26\ février\ 2017.xlsx -o dist-contrib
