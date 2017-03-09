655c7b4bab9399f5ef2a04b051ef485d255356b1
yann <yann.boisselier@gmail.com>
2017-03-09 18:29:24 +0100

    add kane-thornwyrd contributions

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+npm install
+
 python3 processors/python_builder/Main.py
 
-npm install
-node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t src/rapports/2017-02-01--2017-02-26/originaux/Chaines\ Gen\ -\ Période\ du\ 1er\ au\ 26\ février\ 2017.xlsx -o dist-contrib
+node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t src/rapports/2017-02-01--2017-02-26/originaux/Chaines\ Gen\ -\ Période\ du\ 1er\ au\ 26\ février\ 2017.xlsx -o dist/rapports/2017-02-01--2017-02-26
+
