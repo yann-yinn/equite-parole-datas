7919ca7c48d9a7ee4c36c48492e4259bf7bf32be
yann <yann.boisselier@gmail.com>
2017-03-09 09:26:47 +0100

    mise à jour de tous les relevés

--- crawler.sh
+++ crawler.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+# récupérer les fichiers originaux depuis le site du csa
+# et les enregistre dans le dossier src
+
+echo "-----------------"
+echo "Suppression des relevés précédents"
+echo "-----------------"
+rm -rf "src/releves"
+
 for CHAINE in 1 2 3 4 5 6 8 14 15 16 19 24 26 27 101 102 103 104 105 106 107 108 109 110
 do
-  echo "chaine $CHAINE"
+  echo ""
+  echo "-----------------"
+  echo "Chaîne $CHAINE"
+  echo "-----------------"
+  echo ""
   for VARIABLE in 1 2 3 4
   do
     FILE="http://www.csa.fr/csaelections/consultereleve/$CHAINE/$VARIABLE/1?csv=1"
-    echo "fil $FILE"
+    echo "téléchargement du fichier $FILE"
     DESTINATION="src/releves/$CHAINE/$VARIABLE/releve-$VARIABLE.csv";
-    curl $FILE --create-dirs -o $DESTINATION;
-    sleep .$[ ( $RANDOM % 4 ) + 1 ]s
+    curl $FILE --create-dirs --progress-bar -o $DESTINATION;
+    sleep .$[ ( $RANDOM % 4 ) + 3 ]s
   done
 done
 
