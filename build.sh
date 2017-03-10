946c1752835c19d4173032467ec6027323958884
yann <yann.boisselier@gmail.com>
2017-03-10 15:11:51 +0100

    ajout des json de kane-thornwyrd

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ npm update
 # génération des fichiers json
 python3 processors/python_builder/Main.py
 
-node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t "src/releves-par-groupe-de-chaines/2017-02-01--2017-02-26/originaux/Chaines Gen - Période du 1er au 26 février 2017.xlsx" -o "dist/api/2017-02-01--2017-02-26/v1" -p "--chaines-generalistes"
+node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t "src/releves-par-groupe-de-chaines/2017-02-01--2017-02-26/originaux/Chaines Gen - Période du 1er au 26 février 2017.xlsx" -o "dist/api/v1/2017-02-01--2017-02-26/" -p "chaines-generalistes-"
 
