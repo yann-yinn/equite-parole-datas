332744a725ca772cd0d9c5e2b0cd9cedf13b6bd4
yann <yann.boisselier@gmail.com>
2017-03-11 13:12:47 +0100

    fix build with new php parser

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@
 npm install
 npm update
 
-# génération des fichiers json
+# Génération des fichiers csv
 python3 processors/python_builder/Main.py
+
+php processors/php_builder/main.php
 
 node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t "src/releves-par-groupe-de-chaines/2017-02-01--2017-02-26/originaux/Chaines Gen - Période du 1er au 26 février 2017.xlsx" -o "dist/api/v1/2017-02-01--2017-02-26/" -p "chaines-generalistes-"
 
