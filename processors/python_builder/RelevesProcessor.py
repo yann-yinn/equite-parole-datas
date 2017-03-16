4bf172109506dff1a4e7b0958b310c101c69a68b
Bastien SARO <bastien.saro@niji.fr>
2017-03-16 18:12:41 +0100

    [Python] releve-par-candidat

--- processors/python_builder/RelevesProcessor.py
+++ processors/python_builder/RelevesProcessor.py
@@ -5,64 +5,11 @@ from datetime import datetime
 import json
 from Config import Config
 
-class Timeset:
-	pass
-
-class Report:
-	""""""
-	raport = {}
-	categories = {
-		"Total Temps de parole" : "total_temps_de_parole",
-		"Total Temps d'antenne" : "total_temps_antenne",
-	}
-
-	def __init__(self, raport):
-		self.raport = raport
-
-	@staticmethod
-	def createFromCSV(releve):
-		raport = {}
-		lines = releve.read().split('\n')
-
-		for line in lines[1:]:
-			cols = line.split(';')
-			candidat = cols[0].replace('"', '')
-			categorie = Report.categories[cols[1].replace('"', '')]
-			if not candidat in raport:
-				raport[candidat] = {}
-			if not categorie in raport[candidat]:
-				raport[candidat][categorie] = 0
-
-			h, m, s = map(int, cols[2].split(':'))
-			raport[candidat][categorie] += (h * 3600) + (m * 60) + s
-
-		return Report(raport)
-
-	def __operation(self, other, f):
-		if isinstance(other, Report):
-			candidats = list(set(self.raport.keys()).union(other.raport.keys()))
-			for candidat in candidats:
-				if candidat in self.raport and candidat in other.raport:
-					for n, cat in self.categories.items():
-						if cat in self.raport[candidat] and cat in other.raport[candidat]:
-							self.raport[candidat][cat] = f(self.raport[candidat][cat], other.raport[candidat][cat])
-						elif cat in other.raport[candidat]:
-							self.raport[candidat][cat] = other.raport[candidat][cat]
-				elif candidat in other.raport:
-					self.raport[candidat] = other.raport[candidat]
-		else :
-			raise NotImplementedError
-
-	def __add__(self, other):
-		self.__operation(other, lambda a, b: a+b)
-		return self
-
-	def __sub__(self, other):
-		self.__operation(other, lambda a, b: a-b)
-		return self
-
-	def __str__(self):
-		return self.raport.__str__()
+class ReleveJSONEncoder(json.JSONEncoder):
+	def default(self, o):
+		if isinstance(o, Timeset):
+			return o.getData()
+		return json.JSONEncoder.default(self, o)
 
 class RelevesProcessor:
 	config = {}
@@ -81,7 +28,29 @@ class RelevesProcessor:
 		data = RelevesProcessor.crawl_sources(self.config, self.meta)
 
 		with open(self.config['DEST'] + 'releves-par-periode.json', 'w') as output:
-			output.write(json.dumps(data))
+			output.write(json.dumps({'data' : data}, sort_keys=True, cls=ReleveJSONEncoder))
+
+		# compact for TIME -> Candidat
+		periodes = {}
+		for type, chaines in data.items():
+			for chaine, dates in chaines.items():
+				for date, candidats in dates.items():
+					if not date in periodes:
+						periodes[date] = {}
+					for candidat, timeset in candidats.items():
+						if not candidat in periodes[date]:
+							periodes[date][candidat] = timeset
+						else:
+							periodes[date][candidat] += timeset
+
+		for date, candidats in periodes.items():
+			date_dir = self.config['DEST'] + date + '/'
+			if not os.path.isdir(date_dir):
+				os.mkdir(date_dir)
+
+			with open(date_dir + 'releves-par-candidats.json', 'w') as output:
+				output.write(json.dumps({'data': candidats}, sort_keys=True, cls=ReleveJSONEncoder))
+
 
 	@staticmethod
 	def crawl_sources(config, meta):
@@ -143,3 +112,104 @@ class RelevesProcessor:
 				prec = date_raport
 
 		return data
+
+class Timeset:
+	categories = {
+		"Total Temps de parole" : "total_temps_de_parole",
+		"Total Temps d'antenne" : "total_temps_antenne",
+	}
+
+	def __init__(self):
+		self.values = {
+		'total_temps_de_parole' : 0,
+		'total_temps_antenne' : 0
+	}
+
+	def addTo(self, categorie, value):
+		if categorie in Timeset.categories:
+			cat = Timeset.categories[categorie]
+			if not cat in self.values :
+				self.values[cat] = 0
+
+			self.values[cat] += value
+
+	def __operate(self, other, f):
+		if isinstance(other, Timeset):
+			for i, c in Timeset.categories.items():
+				self.values[c] = f(self.values[c], other.values[c])
+			return self
+		else:
+			raise NotImplementedError
+
+	def __add__(self, other):
+		return self.__operate(other, lambda x,y:x+y)
+
+	def __sub__(self, other):
+		return self.__operate(other, lambda x,y:x-y)
+
+	def getData(self):
+		data = {}
+
+		for i, c in Timeset.categories.items():
+			seconds = self.values[c]
+			m, s = divmod(seconds, 60)
+			h, m = divmod(m, 60)
+			time = "%dh %02dmin %02s" % (h, m, s)
+			data[c] = {
+				'secondes' : seconds,
+				'temps' : time
+			}
+
+		return data
+
+	def __str__(self):
+		return self.getData().__str__()
+
+class Report:
+	""""""
+	raport = {}
+
+	def __init__(self, raport):
+		self.raport = raport
+
+	@staticmethod
+	def createFromCSV(releve):
+		raport = {}
+		lines = releve.read().split('\n')
+
+		for line in lines[1:]:
+			cols = line.split(';')
+			candidat = cols[0].replace('"', '')
+			categorie = cols[1].replace('"', '')
+			if not candidat in raport:
+				raport[candidat] = Timeset()
+
+			h, m, s = map(int, cols[2].split(':'))
+			secondes= (h * 3600) + (m * 60) + s
+
+			raport[candidat].addTo(categorie, secondes)
+
+		return Report(raport)
+
+	def __operation(self, other, f):
+		if isinstance(other, Report):
+			candidats = list(set(self.raport.keys()).union(other.raport.keys()))
+			for candidat in candidats:
+				if candidat in self.raport and candidat in other.raport:
+					self.raport[candidat] = f(self.raport[candidat], other.raport[candidat])
+				elif candidat in other.raport:
+					self.raport[candidat] = other.raport[candidat]
+
+		else :
+			raise NotImplementedError
+
+	def __add__(self, other):
+		self.__operation(other, lambda a, b: a+b)
+		return self
+
+	def __sub__(self, other):
+		self.__operation(other, lambda a, b: a-b)
+		return self
+
+	def __str__(self):
+		return self.raport.__str__()
