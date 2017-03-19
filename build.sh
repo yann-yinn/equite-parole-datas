a5c4aba56907612380ccdfc2590a6622384d82c8
yann <yann.boisselier@gmail.com>
2017-03-19 11:40:27 +0100

    udapte all files

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ npm update
 
 # Génération des fichiers JSON et csv par les processors existants
 python3 processors/python_builder/Main.py
-php processors/php_builder/main.php
+#php processors/php_builder/main.php
 node_modules/csa-xlsx-converter/bin/csa-xlsx-converter -t "src/releves-par-groupe-de-chaines/2017-02-01--2017-02-26/originaux/Chaines Gen - Période du 1er au 26 février 2017.xlsx" -o "dist/api/v1/2017-02-01--2017-02-26/" -p "chaines-generalistes-"
 
