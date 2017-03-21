ac5961482f680c48c9463eb1becd3dfb3de76fa2
yann <yann.boisselier@gmail.com>
2017-03-21 14:54:06 +0100

    add comments

--- processors/csa-xlsx-converter/lib/main.js
+++ processors/csa-xlsx-converter/lib/main.js
@@ -6,25 +6,24 @@ const _ = require('lodash');
 module.exports = function(filename) {
   const workbook = XLSX.readFile(filename);
 
+  // ces deux objets seront transformés chacun en un json
   const perPersona = {};
   const perChannel = {};
 
   // on va considérer que tout ce qui n'est pas une de ses clefs sera un prénom - nom
-  // on aura le "Total Candidat" avec, il ne se trouve pas toujours
-  // au même endroit de feuille et ce n'est pas très grave
-  const ROWS = ['Candidat', 'Soutiens', 'Total Temps de parole', "Total Temps d'antenne", "Antenne"]
-
-  //const TOTAL_RECO = /^Total candidats/i;
+  // on aura aussi le "Total Candidat" avec, ce n'est pas très grave.
+  // Ce "Total Candidat" est d'ailleurs parfois au début de la feuille, parfois à la fin...
+  const ROWS = ['Candidat', 'Soutiens', 'Total Temps de parole', "Total Temps d'antenne", "Antenne"];
 
   channelLoop:
   for(const canal of workbook.SheetNames) {
     let persona = '';
-    let truePersona = '';
     let attrs = [];
     let finalattrs = [];
     for (const cell in workbook.Sheets[canal]) {
       if(cell[0] === '!' || cell[0] === 'C' || cell === 'A1' || cell === 'B1' ) continue;
       if(cell[0] === 'A') {
+        // si on est sur une ligne qui contient uniquement le nom du candidat
         if(ROWS.indexOf(workbook.Sheets[canal][cell].v) === -1) {
           if(persona) {
             finalattrs = _.fromPairs(_.chunk(attrs,2));
@@ -34,7 +33,9 @@ module.exports = function(filename) {
           }
           persona = workbook.Sheets[canal][cell].v;
           continue;
-        } else {
+        }
+        // si on est sur une ligne qui ne contient PAS le nom du candidat
+        else {
           attrs.push(workbook.Sheets[canal][cell].v);
           continue;
         }
