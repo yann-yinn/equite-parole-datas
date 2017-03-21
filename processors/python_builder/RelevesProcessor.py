76597953f06d0e89d441e28e10e45babade383d1
Bastien SARO <bastien.saro@niji.fr>
2017-03-21 17:26:57 +0100

    [Python] some comment

--- processors/python_builder/RelevesProcessor.py
+++ processors/python_builder/RelevesProcessor.py
@@ -16,21 +16,33 @@ class RelevesProcessor:
 
 	def __init__(self):
 		""""""
+
+		# Préfix les chemin avec le dossier "root" (chemin absolu vers equite-paroles-datas/"
+		# Puis exporte les config dans la clé config de l'object
 		for key, value in Config.RELEVES.items():
 			if key in ['SRC', 'DEST', 'METADATA']:
 				self.config[key] = Config.ROOT + value
 			else :
 				self.config[key] = value
 
+		# Lecture du fichier de metatdata, il contiens les informations sur les dates des intevales de temps et les
+		# chaines
 		with open(self.config['METADATA'], 'r') as meta_file:
 			self.meta = json.load(meta_file)
 
+		# Fonction de processing des valeurs, retourne un dictonnaire de données complètes
 		data = RelevesProcessor.crawl_sources(self.config, self.meta)
 
+		# écriture des données complètes
+		# ----- : Type de Chaine -> Chaine -> periode -> candidats -> temps
 		with open(self.config['DEST'] + 'releves-par-periode.json', 'w') as output:
 			output.write(json.dumps(data, sort_keys=True, cls=ReleveJSONEncoder))
 
-		# compact for TIME -> Candidat
+		# Réorganiser les données avec un dossier par période de temps, et un fichier avec le rapport de tous les
+		# candidats
+		# ----- : periode/ -> candidats -> temps
+
+		# cumul des temps
 		periodes = {}
 		for type, chaines in data.items():
 			for chaine, dates in chaines.items():
@@ -42,6 +54,8 @@ class RelevesProcessor:
 							periodes[date][candidat] = timeset
 						else:
 							periodes[date][candidat] += timeset
+
+		# Génération d'un fichier de méta données pour les périodes de temps
 		meta_dates = {
 			'periodes' : {}
 		}
@@ -54,14 +68,17 @@ class RelevesProcessor:
 			if not os.path.isdir(date_dir):
 				os.mkdir(date_dir)
 
+			# Ecriture d'un fichier {periode}/releves-par-candidats.json : candidats -> temps
 			with open(date_dir + 'releves-par-candidats.json', 'w') as output:
 				output.write(json.dumps(candidats, sort_keys=True, cls=ReleveJSONEncoder))
 
+		# Ecriture du fichier de méta données
 		with open(self.config['DEST'] + 'releves-hebdomadaires-metadonnees.json', 'w') as output:
 			output.write(json.dumps(meta_dates, sort_keys=True))
 
 	@staticmethod
 	def periodeToReadable(periode):
+		"""Prend un format période {AAAA-MM-JJ}--{AAAA-MM-JJ} et retourne un format lisible en français."""
 		src_format = '%Y-%m-%d'
 		dest_format = '%d/%m/%Y'
 		f, t = periode.split('--')
@@ -72,25 +89,35 @@ class RelevesProcessor:
 
 	@staticmethod
 	def crawl_sources(config, meta):
-		""""""
+		"""Fonction de processing des données de temps pour toute les chaines, tous les intervales de temps, retourne un dictonnaire de données complètes"""
 		folder = config['SRC']
 		data = {}
 
+		# Liste les chaines dans src/releves-par-chaines
 		chaines = [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
 
+		#Parcour des chaines
 		for chaine in chaines:
+
+			# extrait le type et le nom du media via les meta données fournies
 			type = meta['chaines'][chaine]['type']
 			name = meta['chaines'][chaine]['name']
+
 			if not type in data:
 				data[type] = {}
 			data[type][name] = {}
 
+			# Liste les périodes dans chaque dossier de chaine
 			chaine_path = folder + chaine + '/'
 			dates = [d for d in os.listdir(chaine_path) if os.path.isdir(os.path.join(chaine_path, d))]
 
 			organiser = {}
+
+			# Parcour des dates
 			for date in dates:
 				report_date_path = chaine_path + date + '/releve-' + date + '.csv'
+
+				# Lis le fichier de rapport
 				if os.path.isfile(report_date_path):
 					with open(report_date_path, 'r', encoding = config['SRC_ENCODING']) as report:
 						organiser = RelevesProcessor.organise_reports(organiser, date, Report.createFromCSV(report), meta)
