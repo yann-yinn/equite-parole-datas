768fd8d9d855e7607a324e372838e195173ce603
yann <yann.boisselier@gmail.com>
2017-03-21 14:37:25 +0100

    fix nodejs processor for xlsx

--- processors/csa-xlsx-converter/lib/main.js
+++ processors/csa-xlsx-converter/lib/main.js
@@ -11,7 +11,10 @@ module.exports = function(filename) {
 
   const PERSONA_RECOGNITION = /^([A-Z\s]+)(?:\s\(.+\))?$/;
 
-  const END_INFOS = /^Total candidats/i;
+  // on va considérer que tout ce qui n'est pas une de ses clefs sera un prénom - nom
+  const ROWS = ['Candidat', 'Soutiens', 'Total Temps de parole', "Total Temps d'antenne", "Antenne"]
+
+  //const TOTAL_RECO = /^Total candidats/i;
 
   channelLoop:
   for(const canal of workbook.SheetNames) {
@@ -21,14 +24,12 @@ module.exports = function(filename) {
     let finalattrs = [];
     for (const cell in workbook.Sheets[canal]) {
       if(cell[0] === '!' || cell[0] === 'C' || cell === 'A1' || cell === 'B1' ) continue;
-      //if(END_INFOS.test(workbook.Sheets[canal][cell].v)) continue channelLoop;
       if(cell[0] === 'A') {
-        if(PERSONA_RECOGNITION.test(workbook.Sheets[canal][cell].v)) {
+        if(ROWS.indexOf(workbook.Sheets[canal][cell].v) === -1) {
           if(persona) {
-            truePersona = PERSONA_RECOGNITION.exec(persona)[1];
             finalattrs = _.fromPairs(_.chunk(attrs,2));
-            _.set(perPersona, `${truePersona}.${canal}`, finalattrs);
-            _.set(perChannel, `${canal}.${truePersona}`, finalattrs);
+            _.set(perPersona, `${persona}.${canal}`, finalattrs);
+            _.set(perChannel, `${canal}.${persona}`, finalattrs);
             attrs = [];
           }
           persona = workbook.Sheets[canal][cell].v;
