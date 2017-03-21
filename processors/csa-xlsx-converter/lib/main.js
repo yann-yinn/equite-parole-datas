b490eee222fea2d31d9eb52cf2125ee59bb4b951
yann <yann.boisselier@gmail.com>
2017-03-21 17:34:45 +0100

    standardisation fichier json crachés par nodejs

--- processors/csa-xlsx-converter/lib/main.js
+++ processors/csa-xlsx-converter/lib/main.js
@@ -3,6 +3,27 @@
 const XLSX = require('xlsx');
 const _ = require('lodash');
 
+/**
+ * change "00:01:30" to number of seconds (90 in this case)
+ */
+function hhmmss_to_seconds(string) {
+  const a = string.split(':'); // split it at the colons
+  // minutes are worth 60 seconds. Hours are worth 60 minutes.
+  var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
+  return seconds;
+}
+
+function mapping(value) {
+  const mapping = {
+    Candidat:  'candidat',
+    Soutiens: 'soutiens',
+    "Total Temps de parole":'total_temps_de_parole',
+    Antenne: 'antenne',
+    "Total Temps d'antenne": 'total_temps_antenne',
+  }
+  return mapping[value];
+}
+
 module.exports = function(filename) {
   const workbook = XLSX.readFile(filename);
 
@@ -13,7 +34,7 @@ module.exports = function(filename) {
   // on va considérer que tout ce qui n'est pas une de ses clefs sera un prénom - nom
   // on aura aussi le "Total Candidat" avec, ce n'est pas très grave.
   // Ce "Total Candidat" est d'ailleurs parfois au début de la feuille, parfois à la fin...
-  const ROWS = ['Candidat', 'Soutiens', 'Total Temps de parole', "Total Temps d'antenne", "Antenne"];
+  const KEYS = ['Candidat', 'Soutiens', 'Total Temps de parole', "Total Temps d'antenne", "Antenne"];
 
   channelLoop:
   // le fichier excel contient une feuille par canal / chaine
@@ -23,10 +44,14 @@ module.exports = function(filename) {
     let attrs = [];
     let finalattrs = [];
     for (const cell in workbook.Sheets[canal]) {
+
+      // on ne s'occupe que des deux première collones (A et B), au delà on skip
       if(cell[0] === '!' || cell[0] === 'C' || cell === 'A1' || cell === 'B1' ) continue;
+
+      // La colonne A contient le nom des candidats suivis de leurs attributs
       if(cell[0] === 'A') {
-        // si on arrive sur une ligne qui contient uniquement le nom du candidat
-        if(ROWS.indexOf(workbook.Sheets[canal][cell].v) === -1) {
+        // si on arrive sur une ligne qui contient le nom du candidat
+        if(KEYS.indexOf(workbook.Sheets[canal][cell].v) === -1) {
           if(persona) {
             // il y a parfois des parenthèses qui trainent, on vire.
             // par exemple : François Bayrou (soutien de Macron depuis xxx)
@@ -44,13 +69,22 @@ module.exports = function(filename) {
         }
         // si on est sur une ligne qui ne contient PAS le nom du candidat
         else {
-          attrs.push(workbook.Sheets[canal][cell].v);
+          let value = workbook.Sheets[canal][cell].v;
+          value = mapping(value);
+          attrs.push(value);
           continue;
         }
       }
-      attrs.push(workbook.Sheets[canal][cell].w);
+
+      // la colonne B contient la valeur de nos attributs contenus dans la colonne A
+      if (cell[0] === 'B') {
+        let value = hhmmss_to_seconds(workbook.Sheets[canal][cell].w);
+        attrs.push(value);
+      } 
+
     }
   }
 
   return { perPersona, perChannel };
 }
+
